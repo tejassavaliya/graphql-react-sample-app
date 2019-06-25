5f69b43f9b7fd33d7751792ddd9012124c52e975
tejas.s <tejas.s@ezdi.us>
2019-06-25 13:30:55 +0530

    - Added types and data - create dynamic resolver and createEvent

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
 app.use(
     '/graphql',
     graphqlHttp({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+            
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
             
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
             schema {
                 query: RootQuery
@@ -23,11 +38,18 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ['Romantic Cooking', 'Sailing', 'All Night Coding' ]
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event;
             }
         },
         graphiql: true
@@ -36,3 +58,24 @@ app.use(
 
 
 app.listen(3000);
+
+
+/**
+ * createEvent : you can add data to your object
+  mutation {
+     createEvent(eventInput:{title: "A TEst", description: "this is desc", price: 5665.98, date: "25 june 2019"}) {
+        title
+        description
+    }
+  }
+*/
+
+/**
+ * to see the query data
+ * query {
+      events {
+        title
+        description
+      }
+    }
+ */
