abe3e28fd042dc0df1c49e11234d5b909bdc7cd1
tejas.s <tejas.s@ezdi.us>
2019-06-25 16:47:11 +0530

    - MongoDB Connection - Connect GraphQl and MongoDB

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+const Event = require('./models/event');
 const app = express();
 
 const events = [];
@@ -38,26 +40,48 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                .then(events  => {
+                    return events.map( event => {
+                        return { ...event._doc, _id: event._doc._id.toString() };
+                    });
+                }).catch(err => {
+                    throw err;
+                });
             },
-            createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
+            createEvent: args => {
+                const event = new Event({
+                    title:  args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date)
+                });
+                return event
+                    .save()
+                    .then(result => {
+                        return { ...result._doc };
+                    })
+                    .catch(err => {
+                        console.log("====ERR=====");
+                        throw err;
+                    });
             }
         },
         graphiql: true
     })
 );
 
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+    }@cluster0-flcwu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log("=====ERROR======", err);
+});
 
-app.listen(3000);
+
 
 
 /**
