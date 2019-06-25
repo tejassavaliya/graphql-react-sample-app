2487786e982b9f13f57b2b025a49ea4c2e78b836
tejas.s <tejas.s@ezdi.us>
2019-06-25 18:50:28 +0530

    - Added relations between two user and events

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 const app = express();
 
 const events = [];
@@ -20,18 +22,31 @@ app.use(
                 date: String!
             }
             
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+            
             input EventInput {
                 title: String!
                 description: String!
                 price: Float!
                 date: String!
             }
+            
+            input UserInput {
+                email: String!
+                password: String!
+            }
+            
             type RootQuery {
                 events: [Event!]!
             }
             
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
             schema {
                 query: RootQuery
@@ -54,17 +69,52 @@ app.use(
                     title:  args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '5d1215319067c41e640965ba'
                 });
+                let createdEvent;
                 return event
                     .save()
                     .then(result => {
-                        return { ...result._doc };
+                        createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                        return User.findById('5d1215319067c41e640965ba')
+                    })
+                    .then(user => {
+                        if(!user) {
+                            throw new Error('User not found');
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result => {
+                        return createdEvent;
                     })
                     .catch(err => {
                         console.log("====ERR=====");
                         throw err;
                     });
+            },
+            createUser: args => {
+                return User.findOne( { email: args.userInput.email })
+                    .then( (user) => {
+                        if( user ) {
+                            throw new Error('User already exist');
+                        }
+                        return bcrypt.hash(args.userInput.password, 12);
+                    })
+                    .then(hashedPassword => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword
+                        });
+                        return user.save();
+                    })
+                .then(result => {
+                    console.log("USER RESULT ", result)
+                    return { ...result._doc, _id: result.id };
+                }).catch(err => {
+                    throw err;
+                });
             }
         },
         graphiql: true
--- models/event.js
+++ models/event.js
@@ -18,6 +18,10 @@ const eventSchema = new Schema({
     date: {
         type: String,
         required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 });
 
